141c8e8016e3fcc0b4dcd2b2046ad599461cfd99
Viet Tran <vietnamtran@gmail.com>
2021-02-17 15:35:35 +0000

    Initial commit

--- 0x0C-Javascript_advanced/14-wikipedia.js
+++ 0x0C-Javascript_advanced/14-wikipedia.js
@@ -2,10 +2,20 @@
 
 const createElement = data => {
     let paragraph = document.createElement("P");
-    paragraph.innerHTML(data);
+    paragraph.textContent = data.currentTarget.response;
     document.body.appendChild(paragraph);
 }
 
 const queryWikipedia = callback => {
-    
+    let xhr = new XMLHttpRequest();
+    xhr.open(
+        "GET",
+        "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Stack%20Overflow&origin=*"
+    );
+
+    xhr.send();
+
+    xhr.onload = callback;
 }
+
+queryWikipedia(createElement)
