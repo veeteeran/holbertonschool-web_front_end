b39d4f812ebb588ae8bad4c16b4830b2ffd67ede
Viet Tran <vietnamtran@gmail.com>
2021-02-24 03:35:02 +0000

    Add task 2

--- 0x0E-Cookies_local_storage/src/index.js
+++ 0x0E-Cookies_local_storage/src/index.js
@@ -1,5 +1,3 @@
-// document.getElementById("firstname").onclick = console.log("Clicked")
-
 const setCookies = () => {
     const firstname = document.getElementById("firstname").value;
     const email = document.getElementById("email").value;
@@ -14,6 +12,24 @@ const setCookies = () => {
 const showCookies = () => {
     const cookie = document.cookie;
     const p = document.createElement("p");
-    p.innerHTML = `Cookies: ${cookie}`;
+    const email = getCookie("email");
+    const firstname = getCookie("firstname");
+    p.innerHTML = `Email: ${email} - Firstname: ${firstname}`;
     document.body.append(p);
+}
+
+const getCookie = name => {
+    let cookieName = `${name}=`;
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookieArr = decodedCookie.split(';');
+    for (let i = 0; i < cookieArr.length; i++) {
+        let cookie = cookieArr[i];
+        while (cookie.charAt(0) == ' ') {
+            cookie = cookie.substring(1);
+        }
+        if (cookie.indexOf(name) == 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+        }
+    }
+    return "";
 }
