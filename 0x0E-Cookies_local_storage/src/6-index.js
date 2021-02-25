1a12d23b70678887974733b6c673dc35fb46c433
Viet Tran <vietnamtran@gmail.com>
2021-02-25 04:45:32 +0000

    Add task 6

---
+++ 0x0E-Cookies_local_storage/src/6-index.js
@@ -0,0 +1,34 @@
+const availableItems = ['Shampoo', 'Soap', 'Sponge', 'Water'];
+
+if (window.localStorage !== undefined) {
+    document.addEventListener('DOMContentLoaded', () => {
+        createStore();
+        displayCart();
+    })
+} else {
+    alert('Sorry, your browser does not support Web storage. Try again with a better one');
+}
+
+const addItemToCart = item => {
+    localStorage.setItem(item, 'true');
+}
+
+const createStore = () => {
+    const ul = document.createElement("ul");
+    availableItems.forEach(item => {
+        const li = document.createElement("li");
+        li.innerHTML = item;
+        li.onclick = addItemToCart(item);
+        ul.append(li);
+    })
+    document.body.append(ul);
+}
+
+const displayCart = () => {
+    if (window.localStorage.length > 0) {
+        const p = document.createElement("p");
+        const length = window.localStorage.length;
+        p.innerHTML = `You previously had ${length} items in your cart`;
+        document.body.append(p);
+    }
+}
